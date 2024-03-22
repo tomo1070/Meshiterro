dc2ce40548124c436ef1e621d487e5af20d62d61
tomo1070 <160234199+tomo1070@users.noreply.github.com>
2024-03-22 09:06:52 +0000

    [Finish] 4-15

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :post_images, only: [:new, :create, :index, :show]
-  devise_for :users
+resources :post_images, only: [:new, :create, :index, :show, :destroy]
+ resources :users, only: [:show, :edit, :update]
+devise_for :users
 root to: "homes#top"
 get "/homes/about" => "homes#about", as: "about"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
